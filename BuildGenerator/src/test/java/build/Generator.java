f805f8cd8e2b73501f97ecbe01276ee042e5f57d
Vignesh Balaji <Vignesh Balaji@FFE-Vignesh-Balaji>
2024-02-26 10:01:16 +0530

    Build moved successfully from maven

---
+++ BuildGenerator/src/test/java/build/Generator.java
@@ -0,0 +1,13 @@
+package build;
+
+import org.testng.annotations.Test;
+
+public class Generator {
+
+	@Test
+	public void demoTest() {
+		for(int i=0;i<6;i++) {
+			System.out.println("Build generation successfull");
+		}
+	}
+}
